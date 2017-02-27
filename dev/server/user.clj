ea46f936ce73be9dc13f2051f89e1b5815cca9d5
Chris Murphy <chris@strandz.org>
2017-02-27 16:43:42 +1030

    client project going to need to make into server project

---
+++ dev/server/user.clj
@@ -0,0 +1,29 @@
+(ns user
+  (:require
+    [clojure.pprint :refer (pprint)]
+    [clojure.stacktrace :refer (print-stack-trace)]
+    [clojure.tools.namespace.repl :refer [disable-reload! refresh clear set-refresh-dirs]]
+    [com.stuartsierra.component :as component]
+    [figwheel-sidecar.system :as fig]))
+
+;;FIGWHEEL
+(def figwheel (atom nil))
+
+(defn start-figwheel
+  "Start Figwheel on the given builds, or defaults to build-ids in `figwheel-config`."
+  ([]
+   (let [figwheel-config (fig/fetch-config)
+         props (System/getProperties)
+         all-builds (->> figwheel-config :data :all-builds (mapv :id))]
+     (start-figwheel (keys (select-keys props all-builds)))))
+  ([build-ids]
+   (let [figwheel-config (fig/fetch-config)
+         default-build-ids (-> figwheel-config :data :build-ids)
+         build-ids (if (empty? build-ids) default-build-ids build-ids)
+         preferred-config (assoc-in figwheel-config [:data :build-ids] build-ids)]
+     (reset! figwheel (component/system-map
+                        :figwheel-system (fig/figwheel-system preferred-config)
+                        :css-watcher (fig/css-watcher {:watch-paths ["resources/public/css"]})))
+     (println "STARTING FIGWHEEL ON BUILDS: " build-ids)
+     (swap! figwheel component/start)
+     (fig/cljs-repl (:figwheel-system @figwheel)))))
